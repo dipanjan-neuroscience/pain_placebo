6da7b24dfffc104775be9062d0fee5d65bcbac04
dipanjan-neuroscience <58552772+dipanjan-neuroscience@users.noreply.github.com>
2023-06-25 18:16:41 +0530

    Update wholebraincorrelation.m

--- wholebraincorrelation.m
+++ wholebraincorrelation.m
@@ -23,16 +23,16 @@ clear  subFolders
 
 GLM2_dir=fullfile(root_dir,'GLM2');
 
-%colect left mask file names
+%collect left mask file names
 files = dir('Left');
 files(ismember({files.name},{'.','..'}))=[]; %remove . and ..
 
 for k = 1 : length(files)
-maskNames{k}=files(k).name;
+lMaskNames{k}=files(k).name;
 end
 
-[pathstr, maskNames, ext] = fileparts(maskNames);
-[pathstr, voiNames, ext] = fileparts(maskNames);
+[pathstr, lMaskNames, ext] = fileparts(lMaskNames);
+[pathstr, lVoiNames, ext] = fileparts(lMaskNames);
 
 %maskNames={'lFp1.nii','lSSC.nii','lV1.nii','lA1.nii','lSMA.nii','lMC.nii','laIns.nii','lpIns.nii','lThal.nii','lBroca.nii','rFp1.nii','rSSC.nii','rV1.nii','rA1.nii','rSMA.nii','rMC.nii','raIns.nii','rpIns.nii'};
 %voiNames={'lFp1','lSSC','lV1','lA1','lSMA','lMC','laIns','lpIns','lThal','lBroca','rFp1','rSSC','rV1','rA1','rSMA','rMC','raIns','rpIns'};
@@ -44,14 +44,14 @@ spm_dir = fullfile('GLM2_dir', subNames{sI},'SPM.mat');
 
 for vI = 1: length(voiNames)
 
-mask_dir= fullfile (root_dir, maskNames{vI});
+mask_dir= fullfile (root_dir, lMaskNames{vI});
 
 
 clear matlabbatch;
 matlabbatch{1}.spm.util.voi.spmmat = cellstr(spm_dir); % directory to spm.mat file
 matlabbatch{1}.spm.util.voi.adjust = NaN;
 matlabbatch{1}.spm.util.voi.session = 1; % Session index
-matlabbatch{1}.spm.util.voi.name = voiNames{vI}; % name you want to give 
+matlabbatch{1}.spm.util.voi.name = lVoiNames{vI}; % name you want to give 
 matlabbatch{1}.spm.util.voi.roi{1}.mask.image = cellstr(mask_dir); % directory to mask image
 %matlabbatch{1}.spm.util.voi.roi{1}.mask.threshold = 0.5;
 matlabbatch{1}.spm.util.voi.roi{1}.spm.mask.mtype = 0; % inclusion
@@ -69,17 +69,16 @@ end
 
 
 % calculate correlation matrix among the time series
-[r,p] = corrcoef(t{:,:});
+[lr,lp] = corrcoef(t{:,:});
 %% Fisher's Z transformation of correlation values
 % The Fisher Z-Transformation is a way to transform the sampling distribution of Pearson’s r 
 % (i.e. the correlation coefficient) 
 % so that it becomes normally distributed. The “z” in Fisher Z stands for a z-score.
-z=atanh(r);
 
-
+lz=atanh(lr);
 
 %%add to the multidimensional array
-zmatrix(:,:,sI)=z;
+lzmatrix(:,:,sI)=lz;
 
 cd ..
 cd ..
@@ -89,11 +88,11 @@ end
 
 %% Two smaple t test
 
-[h,p,ci,stats] = ttest2(zmatrix(:,:,m),zmatrix(:,:,n),'Dim',3);
-zmatrix=zmatrix.*h;
+[h,p,ci,stats] = ttest2(lzmatrix(:,:,m),lzmatrix(:,:,n),'Dim',3);
+lzmatrix=lzmatrix.*h;
 
 %% inverse transformation
 
-rmatrix=tanh(zmatrix);
+lrmatrix=tanh(zmatrix);
 
 
